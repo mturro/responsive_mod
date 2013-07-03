8d2b098e1f5ddea180c84c95ef208b3eb3f76bfa
mturro <mturro@mshanken.com>
2013-07-03 14:13:31 -0400

    first commit

---
+++ front-page.php
@@ -0,0 +1,108 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Site Front Page
+ *
+ * Note: You can overwrite front-page.php as well as any other Template in Child Theme.
+ * Create the same file (name) include in /responsive-child-theme/ and you're all set to go!
+ * @see            http://codex.wordpress.org/Child_Themes and
+ *                 http://themeid.com/forum/topic/505/child-theme-example/
+ *
+ * @file           front-page.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2013 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/front-page.php
+ * @link           http://codex.wordpress.org/Template_Hierarchy
+ * @since          available since Release 1.0
+ */
+
+/**
+ * Globalize Theme Options
+ */
+$responsive_options = responsive_get_options();
+/**
+ * If front page is set to display the
+ * blog posts index, include home.php;
+ * otherwise, display static front page
+ * content
+ */
+if ( 'posts' == get_option( 'show_on_front' ) && $responsive_options['front_page'] != 1 ) {
+	get_template_part( 'home' );
+} elseif ( 'page' == get_option( 'show_on_front' ) && $responsive_options['front_page'] != 1 ) {
+	$template = get_post_meta( get_option( 'page_on_front' ), '_wp_page_template', true );
+	$template = ( $template == 'default' ) ? 'index.php' : $template;
+	locate_template( $template, true );
+} else { 
+
+	get_header();
+	
+	//test for first install no database
+	$db = get_option( 'responsive_theme_options' );
+    //test if all options are empty so we can display default text if they are
+    $empty = ( empty( $responsive_options['home_headline'] ) && empty( $responsive_options['home_subheadline'] ) && empty( $responsive_options['home_content_area'] ) ) ? false : true;
+	?>
+
+	<div id="featured" class="grid col-940">
+	
+		<div class="grid col-460">
+
+			<h1 class="featured-title">
+				<?php
+				if ( isset( $responsive_options['home_headline'] ) && $db && $empty )
+					echo $responsive_options['home_headline'];
+				else
+					_e( 'Hello, World!', 'responsive' );
+				?>
+			</h1>
+			
+			<h2 class="featured-subtitle">
+				<?php
+				if ( isset( $responsive_options['home_subheadline'] ) && $db && $empty )
+					echo $responsive_options['home_subheadline'];
+				else
+					_e( 'Your H2 subheadline here', 'responsive' );
+				?>
+			</h2>
+			
+			<p>
+				<?php
+				if ( isset( $responsive_options['home_content_area'] ) && $db && $empty )
+					echo do_shortcode( $responsive_options['home_content_area'] );
+				else
+					_e( 'Your title, subtitle and this very content is editable from Theme Option. Call to Action button and its destination link as well. Image on your right can be an image or even YouTube video if you like.','responsive' );
+				?>
+			</p>
+			
+			<?php if ($responsive_options['cta_button'] == 0): ?>  
+   
+				<div class="call-to-action">
+
+<a class="wepay-widget-button wepay-green button" id="wepay_widget_anchor_51d3350118d70" href="https://www.wepay.com/donations/1777962843">Donate</a><script type="text/javascript">var WePay = WePay || {};WePay.load_widgets = WePay.load_widgets || function() { };WePay.widgets = WePay.widgets || [];WePay.widgets.push( {object_id: 1777962843,widget_type: "donation_campaign",anchor_id: "wepay_widget_anchor_51d3350118d70",widget_options: {list_suggested_donations: true,allow_cover_fee: true,enable_recurring: true,allow_anonymous: true,reference_id: ""}});if (!WePay.script) {WePay.script = document.createElement('script');WePay.script.type = 'text/javascript';WePay.script.async = true;WePay.script.src = 'https://static.wepay.com/min/js/widgets.v2.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(WePay.script, s);} else if (WePay.load_widgets) {WePay.load_widgets();}</script>
+				
+				</div><!-- end of .call-to-action -->
+
+			<?php endif; ?>         
+			
+		</div><!-- end of .col-460 -->
+
+		<div id="featured-image" class="grid col-460 fit">
+		  
+		  <?php $featured_content = ( !empty( $responsive_options['featured_content'] ) ) ? $responsive_options['featured_content'] : '<img class="aligncenter" src="' . get_template_directory_uri() . '/images/featured-image.png" width="440" height="300" alt="" />'; ?>
+							
+			<?php echo do_shortcode( $featured_content ); ?>
+									
+		</div><!-- end of #featured-image --> 
+	
+	</div><!-- end of #featured -->
+               
+	<?php 
+	get_sidebar('home');
+	get_footer(); 
+}
+?>
